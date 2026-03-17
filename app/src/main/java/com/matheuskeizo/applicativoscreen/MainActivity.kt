77be3f2cc18782170251daec6bae0b5621028e38
Matheus Keizo <matheuskeizo@MacBook-Air-de-Matheus.local>
2026-03-17 14:55:53 -0300

    Enviando arquivos do ApplicativoScreen

---
+++ app/src/main/java/com/matheuskeizo/applicativoscreen/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.matheuskeizo.applicativoscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.matheuskeizo.applicativoscreen.screens.LoginScreen
+import com.matheuskeizo.applicativoscreen.screens.MenuScreen
+import com.matheuskeizo.applicativoscreen.screens.PedidosScreen
+import com.matheuskeizo.applicativoscreen.screens.PerfilScreen
+import com.matheuskeizo.applicativoscreen.ui.theme.ApplicativoScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ApplicativoScreenTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    ) {
+                        composable(route = "login") {
+                            LoginScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "menu") {
+                            MenuScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "pedidos") {
+                            PedidosScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "perfil") {
+                            PerfilScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
